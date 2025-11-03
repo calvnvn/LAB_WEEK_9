bdae80568f132abd6ce3e5b73c62548240f9b1dd
Calvin-braus <75593027+Calvin-braus@users.noreply.github.com>
2025-11-03 11:01:37 +0700

    Commit No. 1 – Building Jetpack Compose UI

---
+++ app/src/main/java/com/example/lab_week_9/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.lab_week_9
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn // IMPORT TAMBAHAN
+import androidx.compose.foundation.lazy.items // IMPORT TAMBAHAN
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button // IMPORT TAMBAHAN
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp // IMPORT TAMBAHAN
+import com.example.lab_week_9.ui.theme.LAB_WEEK_9Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LAB_WEEK_9Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val list = listOf("Tanu", "Tina", "Tono")
+                    Home(list)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(items: List<String>) {
+        LazyColumn{
+            item {
+                Column(
+                    modifier = Modifier.padding(16.dp).fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(
+                            id = R.string.enter_item
+                        )
+                    )
+                    TextField(
+                        value = "",
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        ),
+
+                        onValueChange = {
+                        }
+                    )
+
+                    Button(onClick = { }) {
+                        Text(text = stringResource(
+                                id = R.string.button_click
+                            )
+                        )
+                    }
+                }
+            }
+
+            items(items) { item ->
+                Column (
+                    modifier = Modifier.padding(vertical = 4.dp). fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = item)
+                }
+            }
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHome() {
+    LAB_WEEK_9Theme {
+        Home(listOf("Tanu", "Tina", "Tono"))
+    }
+}
